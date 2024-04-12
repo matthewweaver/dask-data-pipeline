03d84e4063c45bcd0d049dad7cde127d403c2657
matthewweaver <matthewweaver17@gmail.com>
2024-04-12 21:34:26 +0100

    part one - transforming softball to golf format; removing class as unnecessary

--- data_munge.py
+++ data_munge.py
@@ -1,5 +1,12 @@
 import pandas as pd
+from us_states_abbreviations import abbreviation_to_name
+name_to_abbreviation = {v: k for k, v in abbreviation_to_name.items()}
 
-
-class Datamunge:
-    
+def read_files():
+    golf = pd.read_csv("unity_golf_club.csv", parse_dates=["dob"])
+    softball = pd.read_csv("us_softball_league.tsv", sep="\t", parse_dates=["date_of_birth"])
+    # Only take first two values incase multiple spaces
+    softball['first_name'] = softball["name"].str.split(expand=True)[0]
+    softball['last_name'] = softball["name"].str.split(expand=True)[1]
+    softball = softball.drop('name', axis=1)
+    softball['us_state'] = softball['us_state'].map(lambda state_name: name_to_abbreviation[state_name])
--- test_data_munge.py
+++ test_data_munge.py
@@ -1,2 +1,1 @@
 import pytest
-from data_munge import Datamunge
