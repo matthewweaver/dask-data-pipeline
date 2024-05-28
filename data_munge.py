739d3ead41026a69070bb8fef3d1f032597b5568
matthewweaver <matthewweaver17@gmail.com>
2024-05-28 18:43:54 +0100

    Added type annotations and checked with mypy; modularised data

--- data_munge.py
+++ data_munge.py
@@ -1,20 +1,21 @@
 import sqlite3
-from dask.utils import tmpfile
-import sqlalchemy
 from contextlib import closing
+from typing import List
 import dask.dataframe as dd
 from us_states_abbreviations import abbreviation_to_name
 
 name_to_abbreviation = {v: k for k, v in abbreviation_to_name.items()}
 
 
-def read_files(filename, separator=None, date_columns=None):
+def read_files(
+    filename: str, separator: str | None = None, date_columns: List[str] | None = None
+) -> dd.DataFrame:
     return dd.read_csv(
         filename, sep=separator, parse_dates=date_columns, engine="python"
     )
 
 
-def transform_softball(softball):
+def transform_softball(softball: dd.DataFrame) -> dd.DataFrame:
     # Standardise first and last name columns - only take first two values incase multiple spaces
     softball["first_name"] = softball["name"].str.split(expand=True, n=2)[0]
     softball["last_name"] = softball["name"].str.split(expand=True, n=2)[1]
@@ -30,9 +31,9 @@ def transform_softball(softball):
     return softball
 
 
-def create_master_dataframe(softball, golf):
+def create_master_dataframe(softball: dd.DataFrame, golf: dd.DataFrame) -> dd.DataFrame:
     master = dd.concat([softball, golf], ignore_index=True)
-    companies = read_files("companies.csv")
+    companies = read_files("data/companies.csv")
     master_with_company_name = master.join(companies.set_index("id"), on="company_id")
     master_with_company_name = master_with_company_name.rename(
         columns={"name": "company_name"}
@@ -41,7 +42,7 @@ def create_master_dataframe(softball, golf):
     return master_with_company_name
 
 
-def remove_bad_records(df):
+def remove_bad_records(df: dd.DataFrame) -> tuple[dd.DataFrame, dd.DataFrame]:
     # Create bad records dataframe
     bad_records = df.loc[
         (df["last_active"].dt.year < df["member_since"])
@@ -57,31 +58,31 @@ def remove_bad_records(df):
     return df, bad_records
 
 
-def ingest_into_sql_db(**dfs):
+def ingest_into_sql_db(table_name: str, df: dd.DataFrame) -> None:
     # Creates sql database file in current working directory when run first time
-    with closing(sqlite3.connect("exercise.db")) as connection:
-        for df in dfs:
-            # Ingest pandas/dask dataframes into SQL database
-            dfs[df].to_sql(df, "sqlite:///exercise.db")
+    with closing(sqlite3.connect("exercise.db")):
+        # Ingest pandas/dask dataframes into SQL database
+        df.to_sql(table_name, "sqlite:///exercise.db")
 
 
 def main():
     golf = read_files(
-        filename="unity_golf_club.csv", date_columns=["dob", "last_active"]
+        filename="data/unity_golf_club.csv", date_columns=["dob", "last_active"]
     )
     softball = read_files(
-        filename="us_softball_league.tsv",
+        filename="data/us_softball_league.tsv",
         separator="\t",
         date_columns=["date_of_birth", "last_active"],
     )
     softball = transform_softball(softball)
-    softball["source_file"] = "us_softball_league.tsv"
-    golf["source_file"] = "unity_golf_club.csv"
+    softball["source_file"] = "data/us_softball_league.tsv"
+    golf["source_file"] = "data/unity_golf_club.csv"
     master = create_master_dataframe(softball, golf)
     master, bad_records = remove_bad_records(master)
     print(master.head())
     print(master.info())
-    ingest_into_sql_db(master=master, bad_records=bad_records)
+    ingest_into_sql_db("master", master)
+    ingest_into_sql_db("bad_records", bad_records)
 
 
 if __name__ == "__main__":
--- test_data_munge.py
+++ test_data_munge.py
@@ -3,23 +3,59 @@ import pytest
 import pandas as pd
 import datetime
 
+
 def test_read_csv():
-    expected_top_row = {'first_name': ['Robert'], 'last_name': ['Mclaughlin'], 'dob': ['1967-03-26'], 'company_id': [3], 'last_active': ['2018-08-25'], 'score': [57], 'member_since': [2013], 'state': ['OR']}
+    expected_top_row = {
+        "first_name": ["Robert"],
+        "last_name": ["Mclaughlin"],
+        "dob": ["1967-03-26"],
+        "company_id": [3],
+        "last_active": ["2018-08-25"],
+        "score": [57],
+        "member_since": [2013],
+        "state": ["OR"],
+    }
     test_df = pd.DataFrame.from_dict(expected_top_row)
-    test_df[["dob", "last_active"]] = test_df[["dob", "last_active"]].apply(pd.to_datetime)
-    golf = read_files(filename="unity_golf_club.csv", date_columns=['dob', 'last_active'])
+    test_df[["dob", "last_active"]] = test_df[["dob", "last_active"]].apply(
+        pd.to_datetime
+    )
+    golf = read_files(
+        filename="data/unity_golf_club.csv", date_columns=["dob", "last_active"]
+    )
     pd.testing.assert_frame_equal(test_df, golf.head(1))
 
+
 def test_read_tsv():
-    expected_top_row = {'name': ['Mikayla Brennan'], 'date_of_birth': ['1966-11-02'], 'company_id': [2], 'last_active': ['2018-07-04'], 'score': [84], 'joined_league': [1989], 'us_state': ['Illinois']}
+    expected_top_row = {
+        "name": ["Mikayla Brennan"],
+        "date_of_birth": ["1966-11-02"],
+        "company_id": [2],
+        "last_active": ["2018-07-04"],
+        "score": [84],
+        "joined_league": [1989],
+        "us_state": ["Illinois"],
+    }
     test_df = pd.DataFrame.from_dict(expected_top_row)
-    test_df[["date_of_birth", "last_active"]] = test_df[["date_of_birth", "last_active"]].apply(pd.to_datetime)
-    softball = read_files(filename="us_softball_league.tsv", separator="\t", date_columns=['date_of_birth', 'last_active'])
+    test_df[["date_of_birth", "last_active"]] = test_df[
+        ["date_of_birth", "last_active"]
+    ].apply(pd.to_datetime)
+    softball = read_files(
+        filename="data/us_softball_league.tsv",
+        separator="\t",
+        date_columns=["date_of_birth", "last_active"],
+    )
     pd.testing.assert_frame_equal(test_df, softball.head(1))
 
+
 def test_transform_softball():
-    softball = read_files(filename="us_softball_league.tsv", separator="\t", date_columns=['date_of_birth', 'last_active'])
+    softball = read_files(
+        filename="data/us_softball_league.tsv",
+        separator="\t",
+        date_columns=["date_of_birth", "last_active"],
+    )
     softball = transform_softball(softball)
+
+
 # def test_create_master_dataframe():
 
 # def test_remove_bad_records():
